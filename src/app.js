0862f42ec7769859b99918d5820cdb06bc522758
Gustavo Granato Silva <gustavo.granato@hotmail.com>
2018-07-31 18:13:46 -0300

    Adicionando arquivos de configuração e arquivo main

---
+++ src/app.js
@@ -0,0 +1,47 @@
+'use strict'
+const Telegram = require('telegram-node-bot')
+const TelegramBaseController = Telegram.TelegramBaseController
+const TextCommand = Telegram.TextCommand
+const chatbot = new Telegram.Telegram('644737311:AAFWlmXHzCd7n840AeeU5mLkJnXZ_LthAlk')
+class EventsController extends TelegramBaseController {
+  vagasUdiAction(scope) {
+    let msg = `Sankhya - Analista de Sistemas Jr. - Contato: contato@sankhya.com.br
+
+Sankhya - Analista de Sistemas Jr. 
+Contato: contato@sankhya.com.br
+
+Cedro - Programador Sr. 
+Contato: th@cedro.com.br
+
+Cedro - Analista de Negócios Jr/Pl/Sr/Es.  
+Contato: th@cedro.com.br
+
+Everis - Programador Jr. 
+Contato: gustavo.granato.silva@everis.com
+
+Everis - Estagiário 
+Contato: gustavo.granato.silva@everis.com
+
+Everis - Gerente de Projetos 
+Contato: gustavo.granato.silva@everis.com
+
+Softbox - Desenvolvedor Front-end 
+Contato: softbox@softbox.com.br
+
+Neppo - Analista de Sistemas 
+Contato: gupy.neppo.com.br`
+    
+        scope.sendMessage(msg);
+  }
+
+get routes() {
+    return {
+      'vagasUdi': 'vagasUdiAction'
+      
+    }
+  }
+}
+chatbot.router
+       .when(
+         new TextCommand('/vagasudi', 'vagasUdi'), new EventsController()
+       )
